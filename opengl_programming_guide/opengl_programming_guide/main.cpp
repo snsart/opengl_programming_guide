bfe05236a1a58d1fa5a0399f10331a0c16e90911
snsart <snsart@163.com>
2023-10-06 15:27:27 +0800

    create

---
+++ opengl_programming_guide/opengl_programming_guide/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  opengl_programming_guide
+//
+//  Created by snsart on 2023/10/6.
+//
+
+#include <iostream>
+
+void init(){
+    
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+
+
